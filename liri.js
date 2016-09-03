3dfe4297340cf8b4bcd1a763d44fd730b54f0a7a
Antonio Montalvo <montalvocode@yahoo.com>
2016-09-03 10:06:31 -0700

    set liri.js with npm export and require modules

---
+++ liri.js
@@ -0,0 +1,6 @@
+//Using the require package we access other dependencies. 
+//npm install export 	//npm install require
+var myTweets = require('./keys.js');
+// console.log(myTweets);
+
+//node liri.js 
